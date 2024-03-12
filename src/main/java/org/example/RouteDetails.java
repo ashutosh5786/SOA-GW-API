bbebf6e50c2c50d8df2f4b502204d3b41b9c3dfc
Ashutosh Singh <ashutosh5786@outlook.com>
2024-03-12 14:49:45 +0000

    Done

--- src/main/java/org/example/RouteDetails.java
+++ src/main/java/org/example/RouteDetails.java
@@ -12,36 +12,4 @@ public class RouteDetails {
         this.endAddress = endAddress;
     }
 
-    public String getEstimatedTimeInSeconds() {
-        return estimatedTimeInSeconds;
-    }
-
-    public void setEstimatedTimeInSeconds(String estimatedTimeInSeconds) {
-        this.estimatedTimeInSeconds = estimatedTimeInSeconds;
-    }
-
-    public String getDistance() {
-        return distance;
-    }
-
-    public void setDistance(String distance) {
-        this.distance = distance;
-    }
-
-    public String getStartAddress() {
-        return startAddress;
-    }
-
-    public void setStartAddress(String startAddress) {
-        this.startAddress = startAddress;
-    }
-
-    public String getEndAddress() {
-        return endAddress;
-    }
-
-    public void setEndAddress(String endAddress) {
-        this.endAddress = endAddress;
-    }
-
 }
